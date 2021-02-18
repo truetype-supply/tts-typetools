0d280f44c976e654eef258eb4406676bbfb78981
ottta <oktamataufik@gmail.com>
2021-02-18 09:35:21 +0700

    [feat] Typetester section

---
+++ apps/www/pages/index.tsx
@@ -0,0 +1,39 @@
+import NextHead from "next/head";
+import { Typetester } from "components/Typetester";
+import { Grid } from "components/Layouts";
+
+export default function Page() {
+    return (
+        <>
+            <NextHead>
+                <title>Typetools by Pulipola</title>
+            </NextHead>
+
+            <main>
+                <Typetester />
+                <Grid
+                    style={{
+                        minHeight: "100vh",
+                    }}
+                >
+                    <div
+                        style={{
+                            borderRight: "1px solid var(--accents-4)",
+                            borderLeft: "1px solid var(--accents-4)",
+                            width: "var(--aside-width)",
+                            backgroundColor: "var(--accents-3)",
+                        }}
+                    />
+                    <div
+                        style={{
+                            borderLeft: "1px solid var(--accents-4)",
+                            backgroundColor: "var(--accents-3)",
+                        }}
+                    >
+                        Glyph Display
+                    </div>
+                </Grid>
+            </main>
+        </>
+    );
+}
